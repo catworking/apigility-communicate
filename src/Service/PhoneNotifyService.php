2fa262b8161f79964597fb03d5b0ad2036148bba
yangshinan <yangsn@figo.cn>
2017-01-19 17:25:49 +0800

    改进极光推送的扩展数据对象

--- src/Service/PhoneNotifyService.php
+++ src/Service/PhoneNotifyService.php
@@ -9,6 +9,7 @@ namespace ApigilityCommunicate\Service;
 
 use ApigilityCommunicate\DoctrineEntity\Notification;
 use ApigilityCommunicate\Service\PhoneNotifyServiceAdapter\PhoneNotifyServiceAdapterInterface;
+use ApigilityCommunicate\V1\Rest\Notification\NotificationEntity;
 use Zend\ServiceManager\ServiceManager;
 use Zend\Hydrator\ClassMethods as ClassMethodsHydrator;
 
@@ -21,10 +22,16 @@ class PhoneNotifyService
      */
     protected $adapter;
 
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceManager;
+
     public function __construct(ServiceManager $services, PhoneNotifyServiceAdapterInterface $adapter)
     {
         $this->classMethodsHydrator = new ClassMethodsHydrator();
         $this->adapter = $adapter;
+        $this->serviceManager = $services;
     }
 
     /**
@@ -41,7 +48,7 @@ class PhoneNotifyService
         if ($this->adapter->pushNotificationAndMessageByAlias(
             $notification->getTitle(),
             $notification->getContent(),
-            $this->classMethodsHydrator->extract($notification),
+            $this->classMethodsHydrator->extract(new NotificationEntity($notification, $this->serviceManager)),
             $notification->getUser()->getId())) {
 
             $callback();
